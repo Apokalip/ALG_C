ac21602d41f6f2f47839adb037dec97caba46ff3
Simeon Zahariev <simeon.zahariev@chaosgroup.com>
2022-05-31 01:58:00 +0300

    Fixing identations

--- src/encoding/encoding_funcs.rs
+++ src/encoding/encoding_funcs.rs
@@ -4,48 +4,41 @@ use std::thread;
 //helper method for vec procedure
 fn find_subsequence(haystack: &[char], needle: &[char]) -> Option<usize>
 {	
-	haystack.windows(needle.len()).rposition(|window| window == needle)
+    haystack.windows(needle.len()).rposition(|window| window == needle)
 }
 
 // Vector based solution, It is on avarage 20-30% faster
 // Ofcourse Graphemes would be simply better memory wise and speed wise, but interview tests are no place for external libs/mods/crates.
 fn encode_alg_c_vec_proc(input_data: &[char], look_ahead: usize) -> Option<Vec<EncodedChunk>>{
 
+    let data_len = input_data.len();
     // search buffer
-    let mut sbuffer: Vec<char> = vec![];
+    let mut sbuffer: Vec<char> = Vec::with_capacity(data_len);
     // result from the encoding
     let mut encoded_data: Vec<EncodedChunk> = vec![];
     // main iterator
     let mut i = 0;
 
-	let data_len = input_data.len();
-
-	//keeping track of byte len so we dont call iterators every step
-	let mut current_byte_len = 0;
-
     while i < data_len {
-		
         // if even the first char is not found
         let first_char = input_data[i];
-
-		current_byte_len += first_char.len_utf8();
         if !sbuffer.contains(&first_char){
             encoded_data.push(EncodedChunk::new(0,0,first_char));
             sbuffer.push(first_char);
             i += 1;
         } else {
-			//caching patterns and idxs while searching
+            //caching patterns and idxs while searching
             let mut longest_pattern:&[char] = &[];
             let mut sbuffer_pattern_idx = 0;
 
-			// take the most limiting length for the pattern search, be it input string size or look ahead limit,
-			// as the string may not be split perfectly unless the buffersize/lookahead may not be multiples of the length
+            // take the most limiting length for the pattern search, be it input string size or look ahead limit,
+            // as the string may not be split perfectly unless the buffersize/lookahead may not be multiples of the length
             let max_pattern_len = data_len.min(i + look_ahead);
-			//pattern search
+            //pattern search
 
             for pattern_it in i..(max_pattern_len - 1){
-				
-                match (find_subsequence(&sbuffer, &input_data[i..(pattern_it + 1)])){
+                
+                match find_subsequence(&sbuffer, &input_data[i..(pattern_it + 1)]){
                     Some(idx) => {
                         longest_pattern = &input_data[i..(pattern_it + 1)];
                         sbuffer_pattern_idx = idx;
@@ -54,16 +47,16 @@ fn encode_alg_c_vec_proc(input_data: &[char], look_ahead: usize) -> Option<Vec<E
                         break
                     },
                 };
-			}
+            }
             // fill the search buffer with the new found pattern + the last char
             sbuffer.extend(&input_data[i..i +(longest_pattern.len() + 1)]);
 
-			let mut pattern_byte_len: usize = 0;
-			longest_pattern.iter().for_each(|&ch| pattern_byte_len += ch.len_utf8());
+            let mut pattern_byte_len: usize = 0;
+            longest_pattern.iter().for_each(|&ch| pattern_byte_len += ch.len_utf8());
 
-			// pattern offset from the main 'cursor' i
-			let mut pattern_byte_offset: usize = 0;
-			input_data[sbuffer_pattern_idx..i].iter().for_each(|&ch| pattern_byte_offset += ch.len_utf8());
+            // pattern offset from the main 'cursor' i
+            let mut pattern_byte_offset: usize = 0;
+            input_data[sbuffer_pattern_idx..i].iter().for_each(|&ch| pattern_byte_offset += ch.len_utf8());
 
             encoded_data.push(EncodedChunk::new(
                 pattern_byte_offset,
@@ -71,7 +64,7 @@ fn encode_alg_c_vec_proc(input_data: &[char], look_ahead: usize) -> Option<Vec<E
                 input_data[i +(longest_pattern.len())] 
             ));
 
-			//move cursor over after the last char
+            //move cursor over after the last char
             i += longest_pattern.len() + 1;
         }
     }
@@ -97,15 +90,15 @@ fn encode_alg_c_proc(input_string: &str, look_ahead: usize) -> Option<Vec<Encode
             sbuffer.push(first_char);
             i += 1;
         } else {
-			//caching patterns and idxs while searching
+            //caching patterns and idxs while searching
             let mut longest_pattern = "";
             let mut sbuffer_pattern_idx = 0;
 
-			// take the most limiting length for the pattern search, be it input string size or look ahead limit,
-			// as the string may not be split perfectly unless the buffersize/lookahead may not be multiples of the length
+            // take the most limiting length for the pattern search, be it input string size or look ahead limit,
+            // as the string may not be split perfectly unless the buffersize/lookahead may not be multiples of the length
             let max_pattern_len = input_string.len().min(i + look_ahead);
 
-			//pattern search
+            //pattern search
             for pattern_it in i..(max_pattern_len-1){
   
                 match sbuffer.rfind(&input_string[i..(pattern_it+1)]){
@@ -117,7 +110,7 @@ fn encode_alg_c_proc(input_string: &str, look_ahead: usize) -> Option<Vec<Encode
                         break
                     },
                 };
-			}
+            }
 
             // fill the search buffer with the new found pattern + the last char
             sbuffer.push_str(&input_string[i..i +(longest_pattern.len() + 1)]);
@@ -127,7 +120,7 @@ fn encode_alg_c_proc(input_string: &str, look_ahead: usize) -> Option<Vec<Encode
                 longest_pattern.len(),
                 input_string.chars().nth(i +(longest_pattern.len())).unwrap() 
             ));
-			//move cursor over after the last char
+            //move cursor over after the last char
             i += longest_pattern.len() + 1;
         }
     }
@@ -138,56 +131,56 @@ fn encode_alg_c_proc(input_string: &str, look_ahead: usize) -> Option<Vec<Encode
 fn encode_alg_c_mt(input_string: &str, sbuff_len: usize, look_ahead: usize) -> Option<Vec<EncodedChunk>> {
 
 
-	let input_data: Vec<char> = input_string.chars().collect();
+    let input_data: Vec<char> = input_string.chars().collect();
 
-	// num of threads is going to be +1 if there is left over
-	let num_threads = input_data.len() / sbuff_len;
-	//get the left over if we could not split the string into perfect parts
-	let left_over_len = input_data.len() - num_threads*sbuff_len;
+    // num of threads is going to be +1 if there is left over
+    let num_threads = input_data.len() / sbuff_len;
+    //get the left over if we could not split the string into perfect parts
+    let left_over_len = input_data.len() - num_threads*sbuff_len;
 
-	let mut thread_handles = vec![];
+    let mut thread_handles = vec![];
 
-	for i in 0..num_threads{
-		let data_chunk = input_data[i*sbuff_len .. (i+1)*sbuff_len].to_vec();
-       	thread_handles.push(thread::spawn(move || {
-			encode_alg_c_vec_proc(&data_chunk, look_ahead)
+    for i in 0..num_threads{
+        let data_chunk = input_data[i*sbuff_len .. (i+1)*sbuff_len].to_vec();
+           thread_handles.push(thread::spawn(move || {
+            encode_alg_c_vec_proc(&data_chunk, look_ahead)
         }));
-	}
+    }
 
-	// ! We can handle the left over 2 ways, leave it to the last spawned thread but then we need to pass searchbuffer size
-	// to encode_alg_c_proc() which will have more calculations. The other choice is to create an extra thread, which will skip 
-	// the need for search_buffer_size to be passed. Testing showed the second case to be a little fast but neglegable;
-	if left_over_len > 0 {
-		let data_chunk = input_data[sbuff_len*num_threads..].to_vec();
-		thread_handles.push(thread::spawn(move || {
-			encode_alg_c_vec_proc(&data_chunk, look_ahead)
-   		}));
-	}
+    // ! We can handle the left over 2 ways, leave it to the last spawned thread but then we need to pass searchbuffer size
+    // to encode_alg_c_proc() which will have more calculations. The other choice is to create an extra thread, which will skip 
+    // the need for search_buffer_size to be passed. Testing showed the second case to be a little fast but neglegable;
+    if left_over_len > 0 {
+        let data_chunk = input_data[sbuff_len*num_threads..].to_vec();
+        thread_handles.push(thread::spawn(move || {
+            encode_alg_c_vec_proc(&data_chunk, look_ahead)
+           }));
+    }
 
-	// join all threads and collect results
-	let mt_vecs = thread_handles.into_iter().map(|h| h.join().expect("Could not join encoding thread!"));
+    // join all threads and collect results
+    let mt_vecs = thread_handles.into_iter().map(|h| h.join().expect("Could not join encoding thread!"));
 
-	let mut res: Vec<EncodedChunk> = vec![];
-	for v in mt_vecs{
-		res.extend(v.expect("Failed to get vector from thread"));
-	}
-	
-	Some(res)
+    let mut res: Vec<EncodedChunk> = vec![];
+    for v in mt_vecs{
+        res.extend(v.expect("Failed to get vector from thread"));
+    }
+    
+    Some(res)
 }
 
 // actual encoding method, checks for sbuff_len and look_ahead to be ready for procedure
 pub fn encode_alg_c(input_string: &str, sbuff_len: usize, look_ahead: usize) -> Option<Vec<EncodedChunk>> {
-	let cor_sbuff_len = match sbuff_len{
-		len if len < 1 => input_string.len(),
-		_ => sbuff_len
-	};
+    let cor_sbuff_len = match sbuff_len{
+        len if len < 1 => input_string.len(),
+        _ => sbuff_len
+    };
 
-	let cor_look_ahead_len = match look_ahead{
-		len if len < 1 => cor_sbuff_len,
-		_ => look_ahead
-	};
+    let cor_look_ahead_len = match look_ahead{
+        len if len < 1 => cor_sbuff_len,
+        _ => look_ahead
+    };
 
-	encode_alg_c_mt(input_string, cor_sbuff_len, cor_look_ahead_len)
+    encode_alg_c_mt(input_string, cor_sbuff_len, cor_look_ahead_len)
 }
 
 // decoding using primaraly &str so there is no actual iteration and everything is based on the data of the EncodedChunk*s
@@ -200,9 +193,9 @@ pub fn decode_alg_c(input_vec: Vec<EncodedChunk>) -> Option<String>{
         
         //check for invalid encodings
         if res_len < chunk.offset || chunk.offset < chunk.len {
-        	return None
+            return None
         }
-		//str slice params
+        //str slice params
         let start = res_len - chunk.offset;
         let end = start + chunk.len;
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::env;
 use self::encoding::*;
-use std::fs;
 
 mod encoding;
 
